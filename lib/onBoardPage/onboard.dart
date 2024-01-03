22c2557b438fa56a3dc7848e90c342b78f422af3
Kitts Rey Rubio <kittr64@gmail.com>
2024-01-04 01:10:51 +0800

    Revert "Just fixed overflow error on onboard.dart"
    This reverts commit e35e3aee8c25c8c6d5891acbaaa2b52da307102e.

--- lib/onBoardPage/onboard.dart
+++ lib/onBoardPage/onboard.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:practice_login/Components/onBoardpageButton.dart';
 import 'package:practice_login/pages/auth_page.dart';
+import 'package:practice_login/pages/login_page.dart';
+import 'package:practice_login/register_page/register.dart';
+import 'package:practice_login/services/login_register.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class OnBoard extends StatefulWidget {
@@ -24,118 +28,116 @@ class _OnBoard extends State<OnBoard> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView(
-        controller: controller,
-        children: [
-          Container(
-            color: Colors.grey[300],
-            child: const Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                    'Get things done with GiGabay',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  Text(
-                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ultricies sem at iaculis venenatis.',
-                    style: TextStyle(fontSize: 15),
-                    textAlign: TextAlign.center,
-                  )
-                ],
+      body: Container(
+        child: PageView(
+          controller: controller,
+          children: [
+            Container(
+              color: Colors.grey[300],
+              child: const Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Get things done with GiGabay',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    Text(
+                      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ultricies sem at iaculis venenatis.',
+                      style: TextStyle(fontSize: 15),
+                      textAlign: TextAlign.center,
+                    )
+                  ],
+                ),
               ),
             ),
-          ),
-          ListView(
-            children: [
             Container(
               color: Colors.grey[300],
               child: Center(
                   child: Column(children: [
-                    const SizedBox(
+                const SizedBox(
                   height: 100,
                 ),
-                    Image.asset(
+                Image.asset(
                   'images/Avatar1.png',
                   height: 250,
                 ),
-                    const SizedBox(
+                const SizedBox(
                   height: 80,
                 ),
-                    const Text('Get things done with GiGabay',
+                const Text('Get things done with GiGabay',
                     style: TextStyle(fontSize: 20)),
-                    const SizedBox(
+                const SizedBox(
                   height: 10,
                 ),
-                    const Padding(
+                const Padding(
                   padding: EdgeInsets.only(left: 20, right: 20),
-                      child: Text(
+                  child: Text(
                     'GiGabay is a central platform that provides freelancers equal opportunites to showcase their portfolios and reach their target audiences.',
-                      style: TextStyle(fontSize: 12),
-                      textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 12),
+                    textAlign: TextAlign.center,
                   ),
                 ),
-                    const SizedBox(
-                    height: 200,
+                const SizedBox(
+                  height: 200,
                 ),
-                    Container(
-                      height: 40,
-                      width: 300,
-                      color: const Color.fromARGB(255, 124, 210, 231),
-                      child: const Padding(
-                        padding: EdgeInsets.only(top: 10),
-                        child: Text(
-                        'Get Started!!',
-                        style: TextStyle(
-                            color: Colors.black, fontWeight: FontWeight.bold),
-                            textAlign: TextAlign.center,
+                Container(
+                  height: 40,
+                  width: 300,
+                  color: const Color.fromARGB(255, 124, 210, 231),
+                  child: const Padding(
+                    padding: EdgeInsets.only(top: 10),
+                    child: Text(
+                      'Get Started!!',
+                      style: TextStyle(
+                          color: Colors.black, fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.center,
                     ),
                   ),
                 )
               ])),
             ),
-          ]
-          ),
-          Container(
-            color: Colors.grey[300],
-            child: Center(
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const AuthPage()));
-                      },
-                      child: Padding(
-                        padding: const EdgeInsets.only(top: 300),
-                        child: Container(
-                          height: 40,
-                          width: 300,
-                          color: const Color.fromARGB(2255, 124, 210, 231),
-                          child: const Padding(
-                            padding: EdgeInsets.only(top: 10),
-                            child: Text(
-                              'Lets Go',
-                              style: TextStyle(color: Colors.black),
-                              textAlign: TextAlign.center,
+            Container(
+              color: Colors.grey[300],
+              child: Center(
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const AuthPage()));
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.only(top: 300),
+                          child: Container(
+                            height: 40,
+                            width: 300,
+                            color: const Color.fromARGB(2255, 124, 210, 231),
+                            child: const Padding(
+                              padding: EdgeInsets.only(top: 10),
+                              child: Text(
+                                'Lets Go',
+                                style: TextStyle(color: Colors.black),
+                                textAlign: TextAlign.center,
+                              ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                    const Padding(
-                      padding: EdgeInsets.only(top: 300),
-                      child: Text('Already have Account ? Sign In'),
-                    )
-                  ]),
+                      const Padding(
+                        padding: EdgeInsets.only(top: 300),
+                        child: Text('Already have Account ? Sign In'),
+                      )
+                    ]),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
       bottomSheet: Container(
         color: Colors.grey[300],
